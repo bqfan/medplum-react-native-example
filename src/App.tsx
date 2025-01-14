a5ed7a921ef6f2344980ff1f79bec8b7b15a4922
Baiquan Fan <baiquanf.dev@gmail.com>
2025-01-14 11:54:55 +0200

    chore: configure medplum server clientId for local and remote

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ polyfillMedplumWebAPIs();
 const medplum = new MedplumClient({
   // Enter your Medplum connection details here
   // See MedplumClient docs for more details
-  baseUrl: 'http://localhost:8103',
+  // baseUrl: 'http://localhost:8103',
+  baseUrl: 'https://api.medplum.com',
   // ------------------------------------------------------------------------------
   // If you are testing this out with your physical Android / iOS device and not an emulator,
   // you will need to put your computer's local IP address here, for example:
@@ -18,8 +19,9 @@ const medplum = new MedplumClient({
   // Metro will usually emit this address in the line: 'Metro waiting on exp://192.168.1.216:8081'
   // but you will need to change the protocol to 'http://' and the port to 8103 (the Medplum server's default) or whatever port your server is using
   // ------------------------------------------------------------------------------
-  // clientId: 'MY_CLIENT_ID',
-  // projectId: 'MY_PROJECT_ID',
+  // clientId: '2732a287-c0a2-4ee7-a88b-ac00099261c6',
+  clientId: '19018aa7-862a-4fa2-919f-929ca1e0dd9a',
+  // projectId: '857195ee-b6d9-4c79-b449-b7054c10ed50',
   storage: new ExpoClientStorage(),
 });
 
